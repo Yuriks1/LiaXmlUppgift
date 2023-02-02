62a1d0c9b7b7bafad08de57a17ff28cb508712c9
Yuriks1 <juris.adijans@iths.se>
2023-02-02 09:08:10 +0100

    Add CurrencyExchangeRouter

---
+++ src/main/java/se/replyto/microservices/LiaXmlUppgift/routes/CurrencyExchangeRouter.java
@@ -0,0 +1,19 @@
+package se.replyto.microservices.LiaXmlUppgift.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.logging.log4j.LogManager;
+
+public class CurrencyExchangeRouter extends RouteBuilder {
+
+    private static final org.apache.logging.log4j.Logger logger = LogManager.getLogger();
+
+
+    @Override
+    public void configure() throws Exception {
+
+
+
+
+
+    }
+}
