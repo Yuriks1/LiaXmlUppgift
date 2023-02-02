d39c5e7c68b6aef5770b7da7caf62f9d6d9510a9
Yuriks1 <juris.adijans@iths.se>
2023-02-02 09:00:49 +0100

    First commit

---
+++ src/main/java/se/replyto/microservices/LiaXmlUppgift/LiaXmlUppgiftApplication.java
@@ -0,0 +1,13 @@
+package se.replyto.microservices.LiaXmlUppgift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiaXmlUppgiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiaXmlUppgiftApplication.class, args);
+	}
+
+}
