ab24882d6e877876f84e6dfde87d72e1fca40b67
Yuriks1 <juris.adijans@iths.se>
2023-02-03 16:43:11 +0100

    Reformat code

--- src/main/java/se/replyto/microservices/LiaXmlUppgift/routes/CurrencyExchangeRouter.java
+++ src/main/java/se/replyto/microservices/LiaXmlUppgift/routes/CurrencyExchangeRouter.java
@@ -67,7 +67,6 @@ public class CurrencyExchangeRouter extends RouteBuilder {
 
 
         from("direct:csv")
-                .split(body())
                 .doTry()
                 .marshal()
                 .bindy(BindyType.Csv, CurrencyExchangeDto.class)
